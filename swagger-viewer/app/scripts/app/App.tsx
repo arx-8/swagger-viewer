becd49a9ba8942802c6a41f4203af26c1ef448ae
arx-8 <swans.randomness@gmail.com>
2019-03-28 01:03:57 +0900

    wip

--- swagger-viewer/app/scripts/app/App.tsx
+++ swagger-viewer/app/scripts/app/App.tsx
@@ -1,9 +1,12 @@
 import * as React from "react"
+import { RedocStandalone } from "redoc"
 
 export interface AppProps {}
 
-const App: React.SFC<AppProps> = () => {
-  return <>Hello React</>
-}
+const App: React.SFC<AppProps> = () => (
+  <>
+    <RedocStandalone specUrl="https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/yaml/api-with-examples.yaml" />
+  </>
+)
 
 export default App
