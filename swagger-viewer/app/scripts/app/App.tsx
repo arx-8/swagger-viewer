3baf283506f07d8675bf931e540de5b895ae3fb2
arx-8 <swans.randomness@gmail.com>
2019-03-29 00:53:54 +0900

    json ok

--- swagger-viewer/app/scripts/app/App.tsx
+++ swagger-viewer/app/scripts/app/App.tsx
@@ -1,12 +1,14 @@
 import * as React from "react"
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
+import { swaggerStr } from "./swaggerJson"
 
 export interface AppProps {}
 
 const App: React.SFC<AppProps> = () => (
   <>
-    <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+    {/* <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" /> */}
+    <SwaggerUI spec={swaggerStr} />
   </>
 )
 
