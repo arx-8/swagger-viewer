052629485877148d707008bd1b1cc1548f5d95eb
arx-8 <swans.randomness@gmail.com>
2019-03-30 20:49:56 +0900

    yaml str ok

--- swagger-viewer/app/scripts/app/App.tsx
+++ swagger-viewer/app/scripts/app/App.tsx
@@ -1,14 +1,18 @@
+import jsYaml from "js-yaml"
 import * as React from "react"
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
-import { swaggerStr } from "./swaggerJson"
+import { swaggerStr } from "./swaggerYaml"
 
 export interface AppProps {}
+
+const ymlJson = jsYaml.safeLoad(swaggerStr)
+console.log(ymlJson)
 
 const App: React.SFC<AppProps> = () => (
   <>
     {/* <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" /> */}
-    <SwaggerUI spec={swaggerStr} />
+    <SwaggerUI spec={ymlJson} />
   </>
 )
 
