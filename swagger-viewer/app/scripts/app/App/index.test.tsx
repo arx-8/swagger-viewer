f7309c9ad785f77e86f65122eddd2221e1f78163
arx-8 <swans.randomness@gmail.com>
2019-04-21 15:43:07 +0900

    test: Add snapshot tests

--- swagger-viewer/app/scripts/app/App/index.test.tsx
+++ swagger-viewer/app/scripts/app/App/index.test.tsx
@@ -2,6 +2,9 @@ import * as React from "react"
 import { create } from "react-test-renderer"
 import ShallowRenderer from "react-test-renderer/shallow"
 import { App } from "."
+import { swaggerJson_2_0 } from "./fixtures/SwaggerJson_2_0"
+import { swaggerYaml_2_0 } from "./fixtures/SwaggerYaml_2_0"
+import { swaggerYaml_3_0 } from "./fixtures/SwaggerYaml_3_0"
 
 describe("Shallow render test", () => {
   test("shallow render and snapshot", () => {
@@ -22,6 +25,45 @@ describe("Render test", () => {
   test("invalid swagger", () => {
     // ## Arrange ##
     const swaggerJson = ""
+    // ## Act ##
+    const result = create(<App swaggerJson={swaggerJson} />)
+    // ## Assert ##
+    expect(result).toMatchSnapshot()
+  })
+
+  test("Swagger yaml 2.0", () => {
+    // ## Arrange ##
+    const swaggerJson = swaggerYaml_2_0
+    if (!swaggerJson) {
+      fail("Invalid fixture")
+      return
+    }
+    // ## Act ##
+    const result = create(<App swaggerJson={swaggerJson} />)
+    // ## Assert ##
+    expect(result).toMatchSnapshot()
+  })
+
+  test("Swagger yaml 3.0", () => {
+    // ## Arrange ##
+    const swaggerJson = swaggerYaml_3_0
+    if (!swaggerJson) {
+      fail("Invalid fixture")
+      return
+    }
+    // ## Act ##
+    const result = create(<App swaggerJson={swaggerJson} />)
+    // ## Assert ##
+    expect(result).toMatchSnapshot()
+  })
+
+  test("Swagger json 2.0", () => {
+    // ## Arrange ##
+    const swaggerJson = swaggerJson_2_0
+    if (!swaggerJson) {
+      fail("Invalid fixture")
+      return
+    }
     // ## Act ##
     const result = create(<App swaggerJson={swaggerJson} />)
     // ## Assert ##
