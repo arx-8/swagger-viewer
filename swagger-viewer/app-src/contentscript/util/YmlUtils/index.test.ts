4fb18f8941e5675474032d8b2626293d9c48cb88
arx-8 <swans.randomness@gmail.com>
2021-02-20 13:59:42 +0900

    test: Breaking changes

--- swagger-viewer/app-src/contentscript/util/YmlUtils/index.test.ts
+++ swagger-viewer/app-src/contentscript/util/YmlUtils/index.test.ts
@@ -125,10 +125,14 @@ describe("convertToObject / broken string", () => {
       expect(() => {
         convertToObject(src)
       }).toThrowErrorMatchingInlineSnapshot(`
-"unexpected end of the stream within a flow collection at line 6, column 1:
-    
-    ^"
-`)
+        "unexpected end of the stream within a flow collection (6:1)
+
+         3 |   \\"a\\": \\"a\\",
+         4 |   \\"b\\": 2,
+         5 |   \\"cc\\": null,
+         6 | 
+        -----^"
+      `)
     })
 
     it("case:2", () => {
@@ -145,10 +149,16 @@ describe("convertToObject / broken string", () => {
       expect(() => {
         convertToObject(src)
       }).toThrowErrorMatchingInlineSnapshot(`
-"missed comma between flow collection entries at line 4, column 3:
-      \\"b\\": 2,
-      ^"
-`)
+        "missed comma between flow collection entries (4:3)
+
+         1 | 
+         2 | {
+         3 |   \\"a\\": \\"a\\"
+         4 |   \\"b\\": 2,
+        -------^
+         5 |   \\"cc\\": null,
+         6 | }"
+      `)
     })
   })
 
@@ -176,10 +186,16 @@ consumes:
       expect(() => {
         convertToObject(src)
       }).toThrowErrorMatchingInlineSnapshot(`
-"incomplete explicit mapping pair; a key node is missed; or followed by a non-tabulated empty line at line 7, column 21:
-        get: operationId: listVersionsv2
-                        ^"
-`)
+        "bad indentation of a mapping entry (7:21)
+
+         4 |                 title: Simple API overview
+         5 | paths:
+         6 |   /:
+         7 |     get: operationId: listVersionsv2
+        -------------------------^
+         8 |       produces:
+         9 |       - application/json"
+      `)
     })
 
     it("case:2", () => {
@@ -206,10 +222,16 @@ consumes:
       expect(() => {
         convertToObject(src)
       }).toThrowErrorMatchingInlineSnapshot(`
-"bad indentation of a mapping entry at line 9, column 7:
-          produces:
-          ^"
-`)
+        "bad indentation of a mapping entry (9:7)
+
+          6 |   /:
+          7 |     get:
+          8 |               operationId: listVersionsv2
+          9 |       produces:
+        ------------^
+         10 | application/json
+         11 |       responses:"
+      `)
     })
   })
 })
