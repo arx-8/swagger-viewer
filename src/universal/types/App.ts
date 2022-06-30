680481b8ff8b0f12c82ac407ce9f42b00d6acb34
arx-8 <swans.randomness@gmail.com>
2022-07-01 08:22:35 +0900

    fix: bug that also hits the DOM in Swagger-UI due to ambiguous selector

--- src/universal/types/App.ts
+++ src/universal/types/App.ts
@@ -1,4 +1,6 @@
-export const APP_RENDER_ID = "swagger-viewer-render-root"
+export const APP_RENDER_ID = "swagger-viewer--render-root"
+
+export const ORIGINAL_SRC_AREA_CLASS = "swagger-viewer--original_src_area"
 
 export type MessagePayload = {
   type: "CALL_CONVERT_SWAGGER"
