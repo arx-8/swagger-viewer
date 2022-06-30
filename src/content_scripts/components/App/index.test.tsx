f82b1d15c47ed8e42d92c1af4b326d6454041aee
arx-8 <swans.randomness@gmail.com>
2022-06-30 14:32:05 +0900

    refactor: replace react-test-renderer to @testing-library/react

--- src/content_scripts/components/App/index.test.tsx
+++ src/content_scripts/components/App/index.test.tsx
@@ -1,32 +1,11 @@
+import { render } from "@testing-library/react"
 import "jest-styled-components"
 import * as React from "react"
-import { create } from "react-test-renderer"
-import ShallowRenderer from "react-test-renderer/shallow"
 import { convertToObject } from "src/content_scripts/utils/YmlUtils"
 import { App } from "."
 import { swaggerJson_2_0 } from "./fixtures/SwaggerJson_2_0"
 import { swaggerYaml_2_0 } from "./fixtures/SwaggerYaml_2_0"
 import { swaggerYaml_3_0 } from "./fixtures/SwaggerYaml_3_0"
-
-describe("Shallow render test", () => {
-  beforeAll(() => {
-    // Suppress warning about componentWillUpdate
-    console.warn = () => undefined
-  })
-
-  it("shallow render and snapshot", () => {
-    // ## Arrange ##
-    const renderer = ShallowRenderer.createRenderer()
-    const swaggerJson = ""
-
-    // ## Act ##
-    renderer.render(<App swaggerJson={swaggerJson} />)
-    const result = renderer.getRenderOutput()
-
-    // ## Assert ##
-    expect(result).toMatchSnapshot()
-  })
-})
 
 describe("Render test", () => {
   beforeAll(() => {
@@ -37,10 +16,12 @@ describe("Render test", () => {
   it("invalid swagger", () => {
     // ## Arrange ##
     const swaggerJson = ""
+
     // ## Act ##
-    const result = create(<App swaggerJson={swaggerJson} />)
+    const rendered = render(<App swaggerJson={swaggerJson} />)
+
     // ## Assert ##
-    expect(result).toMatchSnapshot()
+    expect(rendered.asFragment()).toMatchSnapshot()
   })
 
   it("Swagger yaml 2.0", () => {
@@ -49,10 +30,12 @@ describe("Render test", () => {
     if (swaggerJson == null) {
       throw new Error("Invalid fixture")
     }
+
     // ## Act ##
-    const result = create(<App swaggerJson={swaggerJson} />)
+    const rendered = render(<App swaggerJson={swaggerJson} />)
+
     // ## Assert ##
-    expect(result).toMatchSnapshot()
+    expect(rendered.asFragment()).toMatchSnapshot()
   })
 
   it("Swagger yaml 3.0", () => {
@@ -61,10 +44,12 @@ describe("Render test", () => {
     if (swaggerJson == null) {
       throw new Error("Invalid fixture")
     }
+
     // ## Act ##
-    const result = create(<App swaggerJson={swaggerJson} />)
+    const rendered = render(<App swaggerJson={swaggerJson} />)
+
     // ## Assert ##
-    expect(result).toMatchSnapshot()
+    expect(rendered.asFragment()).toMatchSnapshot()
   })
 
   it("Swagger json 2.0", () => {
@@ -73,9 +58,11 @@ describe("Render test", () => {
     if (swaggerJson == null) {
       throw new Error("Invalid fixture")
     }
+
     // ## Act ##
-    const result = create(<App swaggerJson={swaggerJson} />)
+    const rendered = render(<App swaggerJson={swaggerJson} />)
+
     // ## Assert ##
-    expect(result).toMatchSnapshot()
+    expect(rendered.asFragment()).toMatchSnapshot()
   })
 })
